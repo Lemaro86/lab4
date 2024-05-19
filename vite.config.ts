82aae4b0cea8265504e0dbfd2ddc74e8ca7e749b
rponomarev <rponomarev@webinar.ru>
2024-05-19 19:51:34 +0300

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    // server: {port: 3000},
     plugins: [react()],
     base: "/lab4",
     server: {
